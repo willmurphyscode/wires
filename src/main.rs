e309a03b1cb854ee6c462a20a15eab867f6f57f8
Will Murphy <willmurphyscode@gmail.com>
2017-08-11 07:59:46 -0400

    prints strings if file was already text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::env;
+
 mod wires;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let result = wires::strings_from_file(&args[1]);
+        match result {
+            Ok(string) => println!("{}", string),
+            Err(msg) => println!("An error occurred: {}", msg)
+        }
+    } else {
+        println!("Please specify a path");
+    }
 }
 
--- src/wires.rs
+++ src/wires.rs
@@ -1,17 +1,42 @@
-pub fn strings_from_file<'a>(path: &str) -> Result<&'a str, &'a str> {
-    
-    Result::Ok("Hello, world")
+use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+
+pub fn strings_from_file<'a>(path: &str) -> Result<String, &'a str> {
+    let fs_result = File::open(path);
+    match fs_result {
+        Ok(file) => {
+            let mut reader = BufReader::new(file);
+            let mut contents = String::new();
+            let read_result = reader.read_to_string(&mut contents);
+            match read_result {
+                Ok(_) => {
+                    Result::Ok(contents.to_string())              
+                },
+                Err(error) => {
+                    print!("{:?}", error);                    
+                    Result::Err("Could not read file")
+                }
+            }
+        },
+        Err(error) => {
+            print!("{:?}", error);
+            Result::Err("Could not open file")
+        }
+    }
 }
+
 
 #[test]
 fn it_reads_strings_from_files() {
-    let expected = Result::Ok("Hello, world");
-    assert_eq!(strings_from_file("hello.txt"), expected);
+    let expected = Result::Ok("Hello, world.".to_string());
+    assert_eq!(strings_from_file("./src/hello.txt"), expected);
 }
 
 #[test]
 fn it_raises_if_no_file_exists() {
     let actual = strings_from_file("no such file");
-    let expected = Result::Err("Could not read file at specified path");
+    let expected = Result::Err("Could not open file");
     assert_eq!(expected, actual)
 }
