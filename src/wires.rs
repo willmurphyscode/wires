04245521103ef538499e4e8087315b4263de49ec
Will Murphy <willmurphyscode@gmail.com>
2017-08-13 13:51:07 -0400

    good tests, feeling done for now

--- src/wires.rs
+++ src/wires.rs
@@ -35,29 +35,37 @@ pub fn bytes_to_strings<W: Write>(bytes: &[u8], w:  &mut W) {
 }
 
 
-#[test]
-fn it_reads_strings_from_files() {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    let expected = Result::Ok("Hello, world.".to_string());
-    assert_eq!(strings_from_file("./src/hello.txt", &mut handle), expected);
-}
 
-#[test]
-fn it_raises_if_no_file_exists() {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    let actual = strings_from_file("no such file", &mut handle);
-    let expected = Result::Err("Could not open file");
-    assert_eq!(expected, actual)
-}
 
 #[test]
 fn it_writes_to_the_buffer() {
+    use std::io::Cursor;
+
     let mut cursor = Cursor::new(Vec::new()); 
     bytes_to_strings("hello".as_bytes(), &mut cursor);
     let expected = "hello\n";
     let vec = cursor.into_inner(); 
+    let actual = String::from_utf8(vec).unwrap();
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn it_omits_intermediate_nonsense() {
+    use std::io::Cursor;
+    let mut cursor = Cursor::new(Vec::new());
+
+    let mut bytes : Vec<u8> = Vec::new();
+    for b in "Hello".as_bytes() {
+        bytes.push(*b);
+    }
+    bytes.push(254u8);
+    bytes.push(254u8);
+    for b in "World".as_bytes() {
+        bytes.push(*b);
+    }
+    bytes_to_strings(&bytes, &mut cursor);
+    let expected = "Hello\nWorld\n";
+    let vec = cursor.into_inner();
     let actual = String::from_utf8(vec).unwrap();
     assert_eq!(expected, actual);
 }
