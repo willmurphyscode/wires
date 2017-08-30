c17eed7c91d2005d0f009490a7ede26eab981338
Will Murphy <willmurphyscode@gmail.com>
2017-08-30 07:39:37 -0400

    add write z string till break method

--- src/z_string.rs
+++ src/z_string.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
 use nom::IResult;
+use std::io::{stderr, Write, ErrorKind};
+use std::process;
 
 #[derive(Debug)]
 struct ZWord {
@@ -8,6 +10,19 @@ struct ZWord {
     second: u8,
     third: u8,
     last_bit: u8
+}
+
+impl ZWord {
+    pub fn to_string(&self) -> String {
+        let ZWord { first: a, second: b, third: c, .. } = *self;
+        let chars = vec![
+            char_from_5_bits(a),
+            char_from_5_bits(b),
+            char_from_5_bits(c)
+        ];
+        let s : String = chars.into_iter().collect();   
+        s  
+    }
 }
 
 
@@ -48,17 +63,45 @@ fn char_from_5_bits(fiver: u8) -> char {
 
 fn z_string_fragment(bytes: &[u8]) -> String {
     if let IResult::Done(_, word) = take_z_word(bytes) {
-        let ZWord { first: a, second: b, third: c, .. } = word;
-        let chars = vec![
-            char_from_5_bits(a),
-            char_from_5_bits(b),
-            char_from_5_bits(c)
-        ];
-        let s : String = chars.into_iter().collect();
-        return s;
+       return word.to_string();
     }
     //TODO handle error case
     "NOT IMPLEMENTED".to_string()
+}
+
+fn read_until_break(bytes: &[u8], collection: &mut Vec<ZWord>) {
+    let mut slice = bytes; 
+    loop {
+        let result = take_z_word(slice);
+        if let IResult::Done(rest, word) = result {
+            let should_break = word.last_bit == 1 || rest.len() <= 2; 
+            collection.push(word);
+            slice = rest;
+            if should_break { break; }            
+        }
+    }
+}
+
+pub fn dump_string_until_break<W: Write>(bytes: &[u8], writer: &mut W) {
+    let mut collection : Vec<ZWord> = Vec::new();
+    read_until_break(bytes, &mut collection);
+    let strings : Vec<String> = collection
+        .into_iter()
+        .map(|z_word| z_word.to_string())
+        .collect();
+
+    for s in strings.into_iter() {
+        let write_result = writeln!(writer, "{}", s);
+        if let Err(e) = write_result {
+            match e.kind() {
+                ErrorKind::BrokenPipe => break,
+                    _ => {
+                        writeln!(stderr(), "{}", e).unwrap();
+                        process::exit(1);
+                    }
+            }
+        }
+    }
 }
 
 #[test]
@@ -84,4 +127,22 @@ fn it_can_parse_the() {
     let expected = "the".to_string();
     let actual = z_string_fragment(sl);
     assert_eq!(expected, actual);
+}
+
+#[test]
+fn read_until_break_breaks() {
+    let bytes_for_the = vec! [0b1100_1011u8, 0b0101_0100u8, 0b1100_1011u8, 0b0101_0101u8, 0b1100_1011u8, 0b0101_0101u8];
+    let sl = &bytes_for_the[..];
+    let expected = "thethe".to_string();
+    let mut collection : Vec<ZWord> = Vec::new();
+    read_until_break(sl, &mut collection);
+    println!("{:?}", collection);
+    let strings : Vec<String> = collection
+        .into_iter()
+        .map(|zword| zword.to_string())
+        .collect();
+
+    let actual = strings.concat();
+
+    assert_eq!(expected, actual); 
 }
