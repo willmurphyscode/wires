f91d955f9ee09c56af85fb3b7d6a0c1a0e810b9c
Will Murphy <willmurphyscode@gmail.com>
2017-09-05 20:46:19 -0400

    clean up and rename things

--- src/z_string.rs
+++ src/z_string.rs
@@ -11,7 +11,7 @@ struct ZWord {
     first: u8,
     second: u8,
     third: u8,
-    last_bit: u8
+    word_end_bit: u8
 }
 
 
@@ -40,7 +40,7 @@ impl ZorkStringExtractor {
             let result = take_z_word(slice);
             if let IResult::Done(rest, word) = result {
                 bytes_consumed += 2; 
-                let should_break = word.last_bit == 1 || rest.len() <= 2; 
+                let should_break = word.word_end_bit == 1 || rest.len() <= 2; 
                 collection.push(word);
                 slice = rest;
                 if should_break { break; }            
@@ -114,7 +114,7 @@ named!( take_z_word<&[u8],ZWord>,
                     first: first,
                     second: second,
                     third: third,
-                    last_bit: word_end_bit
+                    word_end_bit: word_end_bit
                 }
             )
         )
@@ -141,55 +141,18 @@ fn char_from_5_bits(fiver: u8, previous_byte: u8) -> char {
     }
 }
 
-/* The other two alphabet tables
-[| " "; "?"; "?"; "?"; "?"; "?"; "A"; "B"; "C"; "D"; "E"; "F"; "G"; "H"; "I"; "J";
-   "K"; "L"; "M"; "N"; "O"; "P"; "Q"; "R"; "S"; "T"; "U"; "V"; "W"; "X"; "Y"; "Z" |];
-[| " "; "?"; "?"; "?"; "?"; "?"; "?"; "\n"; "0"; "1"; "2"; "3"; "4"; "5"; "6"; "7";
-   "8"; "9"; "."; ","; "!"; "?"; "_"; "#"; "'"; "\""; "/"; "\\"; "-"; ":"; "("; ")" |] |]
-*/
-
-// fn z_string_fragment(bytes: &[u8]) -> String {
-//     if let IResult::Done(_, word) = take_z_word(bytes) {
-//        return word.to_string();
-//     }
-//     //TODO handle error case
-//     "NOT IMPLEMENTED".to_string()
-// }
-
 fn read_until_break(bytes: &[u8], collection: &mut Vec<ZWord>) {
     let mut slice = bytes; 
     loop {
         let result = take_z_word(slice);
         if let IResult::Done(rest, word) = result {
-            let should_break = word.last_bit == 1 || rest.len() <= 2; 
+            let should_break = word.word_end_bit == 1 || rest.len() <= 2; 
             collection.push(word);
             slice = rest;
             if should_break { break; }            
         }
     }
 }
-
-// pub fn dump_string_until_break<W: Write>(bytes: &[u8], writer: &mut W) {
-//     let mut collection : Vec<ZWord> = Vec::new();
-//     read_until_break(bytes, &mut collection);
-//     let strings : Vec<String> = collection
-//         .into_iter()
-//         .map(|z_word| z_word.to_string())
-//         .collect();
-
-//     for s in strings.into_iter() {
-//         let write_result = writeln!(writer, "{}", s);
-//         if let Err(e) = write_result {
-//             match e.kind() {
-//                 ErrorKind::BrokenPipe => break,
-//                     _ => {
-//                         writeln!(stderr(), "{}", e).unwrap();
-//                         process::exit(1);
-//                     }
-//             }
-//         }
-//     }
-// }
 
 #[test]
 fn it_takes_bits() {
